f1bf88ed1fd1db5221a8a20e18ba91f1559f0988
Pr3mKum4r <premkumar.singh2021@vitstudent.ac.in>
2023-12-22 18:58:26 +0530

    feat: Add our designs section

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import Image from 'next/image';
 
-type CardProps = {
-    
+interface CardProps {
+  imageUrl: string;
+  title: string;
+  price: string;
+  rating: string;
+}
+
+const Card = ({ imageUrl, title, price, rating }: CardProps) => {
+
+  return (
+    <div className="flex flex-col items-center bg-white rounded-sm overflow-hidden">
+      <Image src={imageUrl} alt={title} width={250} height={250} className="object-cover" />
+      <div className="py-4 px-1">
+        <h5 className="text-md font-poppins font-semibold tracking-tight text-[#373737]">{title}</h5>
+        <div className="flex justify-center items-center">
+          <span className="text-md font-poppins font-medium text-[#454545]">${price}</span>
+          <span className='mx-5'>|</span>
+          <div className="flex items-center">
+            <span className="text-md font-poppins text-[#454545] mr-2">{rating}</span>
+              <svg className={`w-4 h-4 fill-current text-yellow-400`} viewBox="0 0 20 20">
+                <path d="M10 15l-5.878 3.09 1.122-6.545-4.76-4.635 6.58-.957L10 0l2.936 6.953 6.58.957-4.76 4.635 1.122 6.545z" />
+              </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const Card:React.FC<CardProps> = () => {
-    
-    return <div>Test Component</div>
-}
 export default Card;
---
+++ src/app/designs/page.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import Card from '@/Components/Card';
+import Image from 'next/image';
+
+interface Product {
+    id: number;
+    imageUrl: string;
+    title: string;
+    price: string;
+    rating: string;
+}
+
+const products: Product[] = [
+    {
+        id: 1,
+        imageUrl: '/design1.png',
+        title: 'Spread Collar Shirt',
+        price: '48.99',
+        rating: '5.0',
+    },
+    {
+        id: 2,
+        imageUrl: '/design2.png',
+        title: 'White Solid Formal Shirt',
+        price: '39.0',
+        rating: '4.9',
+    },
+    {
+        id: 3,
+        imageUrl: '/design3.png',
+        title: 'Gray Solid Padded Jacket',
+        price: '39.0',
+        rating: '4.8',
+    },
+    {
+        id: 4,
+        imageUrl: '/design4.png',
+        title: 'Printed Loose T-shirt',
+        price: '39.99',
+        rating: '4.7',
+    },
+    {
+        id: 1,
+        imageUrl: '/design1.png',
+        title: 'Spread Collar Shirt',
+        price: '48.99',
+        rating: '5.0',
+    },
+    {
+        id: 2,
+        imageUrl: '/design2.png',
+        title: 'White Solid Formal Shirt',
+        price: '39.0',
+        rating: '4.9',
+    },
+    {
+        id: 3,
+        imageUrl: '/design3.png',
+        title: 'Gray Solid Padded Jacket',
+        price: '39.0',
+        rating: '4.8',
+    },
+    {
+        id: 4,
+        imageUrl: '/design4.png',
+        title: 'Printed Loose T-shirt',
+        price: '39.99',
+        rating: '4.7',
+    },
+];
+
+const Home = () => {
+    return (
+        <div className="py-8 px-28">
+            <h2 className="text-3xl font-roboto font-medium text-[#224F34] text-center mb-8">Our designs</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                {products.map((product) => (
+                    <Card
+                        key={product.id}
+                        imageUrl={product.imageUrl}
+                        title={product.title}
+                        price={product.price}
+                        rating={product.rating}
+                    />
+                ))}
+            </div>
+            <div className='flex justify-center'>
+                <button type="button" className="flex items-center px-5 py-2 border rounded-sm border-[#224F34] hover:bg-[#C2EFD4] text-[#224F34]">See all <Image className='ml-3 font-poppins font-medium text-[#224F34]' src='/nextArrow.png' height={20} width={20}/></button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
